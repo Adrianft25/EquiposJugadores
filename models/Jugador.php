55c002bdb946018b8d6571aca1e581df58822340
Adrián Freire Torres <adrianft25@gmail.com>
2023-06-09 18:49:45 +0200

    Arreglo para editar jugador

--- models/Jugador.php
+++ models/Jugador.php
@@ -6,6 +6,7 @@ class Jugador {
     private $ciudad;
     private $numero;
     private $equipo;
+    private $isCapitan;
 
     public function __construct($pdo) {
         $this->pdo = $pdo;
@@ -33,16 +34,19 @@ class Jugador {
 
     // Guardar un nuevo jugador en la base de datos
     public function save() {
-        if ($this->id) {
-            // El jugador ya existe, realizar actualización
-            $stmt = $this->pdo->prepare('UPDATE jugadores SET nombre = ?, ciudad = ?, numero = ?, equipo = ? WHERE id = ?');
-            $stmt->execute([$this->nombre, $this->ciudad, $this->numero, $this->equipo, $this->id]);
-        } else {
-            // El jugador es nuevo, realizar inserción
-            $stmt = $this->pdo->prepare('INSERT INTO jugadores (nombre, ciudad, numero, equipo) VALUES (?, ?, ?, ?)');
-            $stmt->execute([$this->nombre, $this->ciudad, $this->numero, $this->equipo]);
-        }
+              // El jugador es nuevo, realizar inserción
+            $stmt = $this->pdo->prepare('INSERT INTO jugadores (nombre, ciudad, numero, equipo, isCapitan) VALUES (?, ?, ?, ?, ?)');
+            $stmt->execute([$this->nombre, $this->ciudad, $this->numero, $this->equipo, $this->isCapitan]);
+    
     }
+
+
+    //Guardar un jugador modificado
+    public function update($nombre, $ciudad, $numero) {
+        $stmt = $this->pdo->prepare('UPDATE jugadores SET nombre = ?, ciudad = ?, numero = ? WHERE id = ?');
+        $stmt->execute([$nombre, $ciudad, $numero, $this->id]);
+    }
+    
 
     // Eliminar un jugador de la base de datos por su ID
     public static function deleteById($pdo, $id) {
@@ -54,6 +58,10 @@ class Jugador {
 
     public function getId() {
         return $this->id;
+    }
+
+    public function setId($id){
+        $this->id = $id;
     }
 
     public function getNombre() {
@@ -86,6 +94,14 @@ class Jugador {
 
     public function setEquipo($equipo) {
         $this->equipo = $equipo;
+    }
+
+    public function getCapitan() {
+        return $this->isCapitan;
+    }
+
+    public function setCapitan($isCapitan) {
+        $this->isCapitan = $isCapitan;
     }
 }
 ?>
--- views/add_jugador.php
+++ views/add_jugador.php
@@ -1,16 +1,72 @@
-<h2>Crear Jugador</h2>
-<form id="jugadorForm" method="POST" action="add_jugador.php">
+
+<?php
+
+
+require_once '../equipo/config.php';
+require_once '../models/Equipo.php';
+require_once '../models/Jugador.php';
+
+    // Obtener el ID del equipo desde la URL
+    $id = $_GET['id'];
+
+    // Buscar el equipo por su ID
+    $jugador = Jugador::getById($pdo, $id);
+
+    if ($jugador) {
+        $jugadorObj = new Jugador($pdo);
+        $jugadorObj->setId($jugador['id']);
+        $jugadorObj->setNombre($jugador['nombre']);
+        $jugadorObj->setCiudad($jugador['ciudad']);
+        $jugadorObj->setNumero($jugador['numero']);
+        $jugadorObj->setEquipo($jugador['equipo']);
+
+        echo "<h2>Nombre: " . $jugadorObj->getNombre() . "</h2>";
+        echo "<p>Ciudad: " . $jugadorObj->getCiudad() . "</p>";
+        echo "<p>Numero: " . $jugadorObj->getNumero() . "</p>"; 
+        echo "<p>Equipo: " . $jugadorObj->getEquipo() . "</p>";
+
+    } else {
+        echo "<p>Equipo no encontrado.</p>";
+    }
+
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // Obtener los datos del formulario
+        $nombre = $_POST['nombre'];
+        $ciudad = $_POST['ciudad'];
+        $numero = $_POST['numero'];
+        $equipo = $_POST['equipo'];
+
+        // Crear el nuevo equipo
+        $j = new Jugador($pdo);
+        $j->setId($id);
+        $j->setNombre($nombre);
+        $j->setCiudad($ciudad);
+        $j->setNumero($numero);
+
+        $j->update($nombre, $ciudad, $numero);
+
+        // Redireccionar a la página de inicio
+        header('Location: ../views/informacion.php?id=' . $equipo);
+        exit();
+    }
+    
+
+    ?>
+
+<h2>Editar Jugador</h2>
+<form id="jugadorForm" method="POST">
     <label for="nombre">Nombre:</label>
-    <input type="text" name="nombre" id="nombre" required>
+    <input type="text" name="nombre" id="nombre" value="<?php echo $jugadorObj->getNombre(); ?>" required>
 
     <label for="ciudad">Ciudad:</label>
-    <input type="text" name="ciudad" id="ciudad">
+    <input type="text" name="ciudad" id="ciudad"  value="<?php echo $jugadorObj->getCiudad(); ?>">
 
     <label for="numero">Numero:</label>
-    <input type="text" name="numero" id="Numero" required>
+    <input type="number" name="numero" id="Numero"  value="<?php echo $jugadorObj->getNumero(); ?>" required>
 
-    <label for="equipo">Equipo:</label>
-    <input type="text" name="equipo" id="equipo" required>
+    <input type="hidden" name="equipo" id="equipo"  value="<?php echo $jugadorObj->getEquipo(); ?>">
 
-    <button type="submit">Crear</button>
+    <button type="submit">Editar</button>
 </form>
+
