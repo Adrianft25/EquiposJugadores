5f386ba85642fbabf79d319c1e76b77f0fa464f9
Adrián Freire Torres <adrianft25@gmail.com>
2023-06-09 15:53:31 +0200

    Formulario para crear Jugador

---
+++ views/add_jugador.php
@@ -0,0 +1,16 @@
+<h2>Crear Jugador</h2>
+<form id="jugadorForm" method="POST" action="add_jugador.php">
+    <label for="nombre">Nombre:</label>
+    <input type="text" name="nombre" id="nombre" required>
+
+    <label for="ciudad">Ciudad:</label>
+    <input type="text" name="ciudad" id="ciudad">
+
+    <label for="numero">Numero:</label>
+    <input type="text" name="numero" id="Numero" required>
+
+    <label for="equipo">Equipo:</label>
+    <input type="text" name="equipo" id="equipo" required>
+
+    <button type="submit">Crear</button>
+</form>
