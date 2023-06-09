96a23f1ea31ec64e46b9e9ade146523eb5a69316
Adrián Freire Torres <adrianft25@gmail.com>
2023-06-09 16:53:58 +0200

    Update de pagina de inicio

--- views/header.php
+++ views/header.php
@@ -8,11 +8,5 @@
 <body>
     <header>
         <h1>Gestión de Equipos</h1>
-        <nav>
-            <ul>
-                <li><a href="../equipo/index.php">Inicio</a></li>
-                <li><a href="../equipo/add.php">Crear Equipo</a></li>
-            </ul>
-        </nav>
     </header>
     
